56722f7767f1e52535ae327235fcaaaa87e67070
huatoh-byte <huatoh@gmail.com>
2026-09-05 17:58:35 +0700

    Prepare website for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-super-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///project.db'
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
